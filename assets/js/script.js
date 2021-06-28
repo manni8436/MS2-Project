b461ad20e646e338d4679cce11d887f7493ec510
manni8436 <emanuelsilva019@gmail.com>
2021-06-28 07:34:39 +0000

    removed unused css link in head tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,8 @@ function boxFlip() {
     for (let i = 0; i < boxes.length; i++) {
         if (boxes[i].style.backgroundColor = "red") {
             boxes[i].style.backgroundColor = "purple";
-            console.log(boxes[i].style.backgroundColor);
         } else {
-            boxes[i].style.backgroundColor = "red"; // the seems to be something wrong with this code but what???
+            boxes[i].style.backgroundColor = "red";
         }
         console.log(boxes[i].style.backgroundColor);
     } 
