516e8db74cb1bb76b42e49029a0435d1447519a8
manni8436 <emanuelsilva019@gmail.com>
2021-06-29 11:05:57 +0000

    added images to use while making the functionality and added more divs to html to use during my testing of my JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,24 @@ const boxes = document.getElementsByClassName("box");
 
 function boxFlip() {
    for (let i = 0; i < boxes.length; i++) {
-        if (boxes[i].classList.contains("box-purple")) {
-            boxes[i].classList.add("box-red");
-            boxes[i].classList.remove("box-purple");
+        if (boxes[i].classList.contains("box-front")) {
+            boxes[i].classList.add("box-back");
+            boxes[i].classList.remove("box-front");
         } else {
-            boxes[i].classList.add("box-purple");
-            boxes[i].classList.remove("box-red");
+            boxes[i].classList.add("box-front");
+            boxes[i].classList.remove("box-back");
         }
     } 
 }
 
+const images = [
+        ["assets/image/GlyphA.jpg", "ALT-HERE", "box-purple"],
+        ["assets/image/GlyphG.jpg", "ALT-HERE", "box-red"],
+    ];
+
+function randomiseBoxOrder() {
+    amountOfBoxes = boxes.length;
+
+    let randomNumber = Math.floor(Math.random() * amountOfBoxes); // Gets a random number between 0 and the total number of boxes
+    currentBox = boxes[`${randomNumber}`]; // Finds a box in the boxes with that index number
+}
