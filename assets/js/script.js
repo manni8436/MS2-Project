a156a3fbd483037df0691a37454be8113f0f5ddd
manni8436 <emanuelsilva019@gmail.com>
2021-07-04 19:46:13 +0000

    added image for back of card to use during the testing of show and hide, added JS code for card randomization

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// makes cards flip
+const cards = document.getElementsByClassName("card");
+
+function flipCard() {
+   for (let i = 0; i < cards.length; i++) {
+        if (cards[i].classList.contains("card-front")) {
+            cards[i].classList.add("card-back");
+            cards[i].classList.remove("card-front");
+        } else {
+            cards[i].classList.add("card-front");
+            cards[i].classList.remove("card-back");
+        }
+    } 
+}
+
+// make cards random
+const colorArray = [
+        '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		'#FF99E6'
+];
+
+colorArray.sort(() => 0.5 - Math.random());
+
+//add random colors to cards
+const randomcolorcards = document.getElementsByClassName('card');
+
+for (let i = 0; i < colorArray.length; i++){
+    cards[i].style.backgroundColor = colorArray[i];
+}
