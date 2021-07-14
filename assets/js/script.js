7a281fdd6824c889abf86bbdcdea9c5785890e97
manni8436 <emanuelsilva019@gmail.com>
2021-07-14 09:14:58 +0000

    changed colorArray to have 2 copies of the same colour for the checking the matching functionality will work

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,9 @@ for (let i = 0; i < cards.length; i++) {
 //add random colors to cards
 const colorArray = [
         '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-		'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-		'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-		'#FF99E6'
+        '#FF33FF', '#3366E6', '#00B3E6', '#99FF99', '#99FF99',
+        '#FFB399', '#FFFF99', '#3366E6', '#66991A', '#66991A', 
+        '#FF6633'    
 ];
 
 colorArray.sort(() => 0.5 - Math.random());
